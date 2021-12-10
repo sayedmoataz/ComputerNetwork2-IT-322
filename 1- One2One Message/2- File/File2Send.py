f348da4d6f7d02e61d6b00311f7ffa4aec5e7541
Sayed Moataz <46425858+sayedmoataz@users.noreply.github.com>
2021-12-10 20:13:11 +0200

    Update File2Send.py

--- 1- One2One Message/2- File/File2Send.py
+++ 1- One2One Message/2- File/File2Send.py
@@ -1,13 +1,2 @@
-"""
-AF_INET refers to the address-family ipv4.
 
-The SOCK_STREAM means connection-oriented TCP protocol.
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
- 
-ip = socket.gethostbyname('www.google.com')
-
-print(ip)
-"""
-
-print(" Put Me in C:\\User\\sayed... \n Stupied Man")
+print(" Put Me in C:\\User\\PC_NAME ...")
